49daee3b883b077fd05dcdfcb93a75925d6033f8
Victor Yan Pereira e Lima <36238607+Victor7095@users.noreply.github.com>
2020-01-19 15:08:34 -0400

    Interface ok

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar(){
+    return (
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+            
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+            
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      
+    );
+}
+
+export default Sidebar;
